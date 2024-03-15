5440cf7b8aa681743bf4a6705d1fb088a3e44663
Sinzxc <nik.orig@mail.ru>
2024-03-15 12:24:21 +0300

    add mongo

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const CC = require('currency-converter-lt')
 const http = require('http')
 const express = require('express')
+const {MongoClient} = require('mongodb')
+const { connect } = require('http2')
+
+const client=new MongoClient("mongodb://localhost:27017")
+const dbName = 'educateDB'; 
+const collectionName = 'Users';
+
+
 
 const app = express()
 app.get('/', (req, res) => {
     res.send('Home Page!')
 })
 
-app.get('/user/:user/:id', (req, res) => {
+
+app.get('/user/:user/:id', async (req, res) => {
     res.send('user: ' + req.params.user+" ID: " + req.params.id)
-})
+    await connectToDB();
+    await addUser(req.params.user, req.params.id);
+    await disconnectDB();
+});
+
 
 
 app.get('/convert/:from/:to/:amount', (req, res) => {
@@ -29,4 +42,29 @@ app.listen(PORT, (req, res) => {
     console.log("Server is running on port: " + PORT);
 });
 
+async function connectToDB() {
+    try {
+        // Подключение к MongoDB
+        await client.connect();
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.error('Error:', err);
+    } 
+}
+async function disconnectDB(){
+    await client.close();
+    console.log('Disconnected from MongoDB');
+}
 
+async function addUser(name, age) {
+    // Выбор базы данных
+    const db = client.db(dbName);
+
+    // Выбор коллекции (она будет создана, если не существует)
+    const collection = db.collection(collectionName);
+
+    // Вставка документа в коллекцию
+    const document = { name: name, age: age };
+    const result = await collection.insertOne(document);
+    console.log('Document inserted:', document.name," ", document.age);
+}
