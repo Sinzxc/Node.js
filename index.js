1c1c9b916880f197f4c00de16e887ed19ff9fb69
Sinzxc <nik.orig@mail.ru>
2024-03-15 11:45:51 +0300

    use express

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 const CC = require('currency-converter-lt')
 const http = require('http')
+const express = require('express')
+
+const app = express()
+app.get('/', (req, res) => {
+    res.send('Home Page!')
+})
+
+app.get('/user/:user/:id', (req, res) => {
+    res.send('user: ' + req.params.user+" ID: " + req.params.id)
+})
 
 
-let currencyConverter = new CC({from:"USD", to:"RUB", amount:100})
-const server=http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+app.get('/convert/:from/:to/:amount', (req, res) => {
+
+    const amount = parseFloat(req.params.amount);
+
+    let currencyConverter = new CC({from : req.params.from, to : req.params.to, amount :amount})
     currencyConverter.convert().then((response) => {
-        res.end(response.toString());
+        res.send('from ' + req.params.from + '\n to ' + req.params.to + '\n amount ' + req.params.amount + "\nresponse: " + response)
     })
 })
 
+
 const PORT=3000;
 const URL="localhost"
-server.listen(PORT,URL, function () {
-    console.log("listening on   " +URL+":"+PORT);
-})
-
+app.listen(PORT, (req, res) => {
+    console.log("Server is running on port: " + PORT);
+});
 
 
